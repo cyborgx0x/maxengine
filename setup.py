f265e648cc25eae38a4865a0767bc83c0cac27fb
calssiq <leeboykt@gmail.com>
2020-05-25 13:19:32 +0700

    create setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='engine',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='cyborgx0x',
+    author_email='',
+    description=''
+)

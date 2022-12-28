49913b0b34447ba493c9e95449b78ab71e7109c5
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2022-12-28 23:04:58 +0700

    using state machine

--- f_engine/engine.py
+++ f_engine/engine.py
@@ -4,6 +4,7 @@ import os
 class Engine():
     def __init__(self, data):
         self._data = data
+        self.startState = None
     def run_backtest(self):
         for row in self._data:
             print("loop through the data")
@@ -11,7 +12,14 @@ class Engine():
     def execute_trade(self, signal: Signal) -> None:
         
         print(signal.get_signal())
-    def plot(self):
-        pass
-    def cache(self):
-        pass
+    def start(self):
+        self.state = self.startState
+    def step(self, newInput):
+        (s, o) = self.getNextValue(self.state, newInput)
+        self.state = s
+        return o
+    def getNextValue(self, state, newState):
+        return state, newState
+    def tranduce(self, inputs):
+        self.start()
+        return [self.step(inp) for inp in inputs]

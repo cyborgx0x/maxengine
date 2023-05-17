4651318b66c028b2e46e995a54866ab52e8df996
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2023-05-17 15:17:18 +0700

    add position controller

---
+++ docs/market_sentiment.md
@@ -0,0 +1,5 @@
+Khái niệm market sentiment được trình bày rất nhiều trong intermaket analysis. 
+Dòng tiền thay đổi ở một thị trường có khả năng thể hiện ra một cái sentiment của thị trường.
+
+Ví dụ: Theo dõi dòng tiền của bond market đem đến cái nhìn về viễn cảnh cũng như triển vọng kinh tế của thị trường.
+Từ đó mà có thể lựa chọn các instrument liên quan chặt chẽ tới sự thay đổi trên bond market như là gold.
--- mt5_bot.py
+++ mt5_bot.py
@@ -21,10 +21,90 @@ def complex_strategy(engine: Engine, instrument:str)->None:
     engine.signal_set.append(MultiTimeframeTrendFollowing(symbol=instrument))
     engine.execute_trade(mt5=True)
     engine.signal_set.clear()
+
+pairs = []
+trading_pair = []
+
+for pair in pairs:
+    if pair.spread > 20:
+        continue
+    else:
+        trading_pair.append(pair)
+
+class Executor():
+    position = []
     
+    @property
+    def pnl(self):
+        return self.calculate_pnl()
+
+class Server():
+    exness = ""
+
+
+class PairCondition():
+    spread = 0
+    
+    
+    def __init__(self) -> None:
+        pass
+    
+    @property
+    def volatility(self):
+        return 
+    
+    @property
+    def volatility_threshold(self):
+        '''
+        Update the threshold calculation here. 
+        By taking the volatility range, the function can return the threshold.
+        Therefore the agent can detemine the quality of current environment
+        '''
+        pass
+
+    @property
+    def confident_score(self):
+        return self.calculate_confident()
+    
+
+    @staticmethod
+    def calculate_confident(self):
+        pass
+    
+    @property
+    def is_ok(self, spread, volatility) -> bool:
+        if self.spread > 20:
+            return False
+        if self.volatility > self.volatility_threshold:
+            return False
+        
+class Pair():
+    condition = PairCondition()
+    
+class MarketCondition():
+    @property
+    def is_ok(self):
+        pass
 
 all_pair = mt5.get_all_pair()
 
 for i in all_pair:
     complex_strategy(engine, i)
+
+def check_condition():
+    '''
+    Combine all criteria and return if the condition is good for trading or not.
+    '''
+
+    market = MarketCondition()
+    pair = Pair()
+    condition = []
+    condition.append(
+        pair.condition.is_ok,
+        market.is_ok
+    )
+    should_i = bool(condition)
+    return should_i
+
+
 

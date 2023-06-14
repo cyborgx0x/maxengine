386a35379223636d47dbaeabd737db46de012beb
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2023-06-14 15:33:45 +0700

    add risk control

--- mt5_bot.py
+++ mt5_bot.py
@@ -78,13 +78,39 @@ class PairCondition():
         if self.volatility > self.volatility_threshold:
             return False
         
+    def __repr__(self) -> str:
+        return f'''
+        Current Pair Condition:
+        {self.is_ok}
+        With the following confident score:
+        {self.confident_score}
+        '''
+        
 class Pair():
     condition = PairCondition()
     
 class MarketCondition():
+    
     @property
     def is_ok(self):
+        return None
+
+    @property
+    def confident_score(self):
+        return self.calculate_confident()
+
+    def calculate_confident(self):
+        return None
+
+    @property
+    def market_score(self):
         pass
+
+    def __repr__(self) -> str:
+        return f'''
+        Current Market Condition: {self.is_ok}
+        Confident Score: {self.confident_score}
+        '''
 
 all_pair = mt5.get_all_pair()
 
@@ -103,8 +129,35 @@ def check_condition():
         pair.condition.is_ok,
         market.is_ok
     )
+    print(market)
+
     should_i = bool(condition)
     return should_i
 
+'''
+After all the analysis is the capital management process
+We have to make sure to check the risk management even with 99.99% percent confident score of analysis
+The following code will implement the 5% rule when it come to risk management. 
+But even with tight stop and 5% rule, it can not protect your capital from extreme events,
+In these kind of event, the lack of liquidity will kill your position in the disfavor. 
+Make sure to locate your capital several business that you know well so you can recover later.
+'''
 
+class RiskControl():
+    available_capital = 0
+    margin_rate = 0
+    current_position = []
 
+    @property
+    def risk_score(self):
+        return self.calculate_risk_score()
+    
+    def calculate_risk_score(self):
+        pass
+
+    @property
+    def available_capital(self):
+        return self.calculate_available_capital()
+    
+
+    
